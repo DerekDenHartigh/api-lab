2da9448fdb1cdb2ea7743cbec1b8cecfcbfe0b8e
Derek <derekdenhartigh@gmail.com>
2019-03-16 19:01:26 -0400

    working on getting the links, also, jquery styling isn't working on appended elements

--- main.js
+++ main.js
@@ -1,27 +1,84 @@
 "use strict";
+/*To Do:
+
+Task:  Create a webpage that displays the posts from the AWW Subreddit.
+Build Speciﬁcations:
+1. Use jQuery to request data from the  https://www.reddit.com/r/aww/.json  API.  
+2. Display the results on the page.  
+    3. At a minimum, include a title, image and working link for each post. 
+    (Easiest is to use the  thumbnail for the image, but there are other images in the JSON response.)
+    4. Make it look good with some CSS.    
+    Bonus  Only show the ﬁrst ten results.  
+*/
 $(()=>{
     $.get("https://www.reddit.com/r/aww/new.json", (data)=>{
         console.log(data);
-
-        // let i=0;  // don't work
-        // while (data.data.children[i]<=10) {
-        //     title = child[i].data.title;
-        //     thumb = child[i].data.thumbnail;
-        //     $("body").append(`<h1>${title}</h1>`);
-        //     $("body").append(`<img src="${thumb}"/>`);
-        //     i++;
-        // }
-
-        let title, thumb, gif;
+        let title, thumb, link;
         let child = data.data.children;
-        let n=0; 
-        data.data.children.forEach((child)=> { // log ALL the titles
-            title = child.data.title;
-            thumb = child.data.thumbnail;
-            $("body").append(`<h1>${title}</h1>`);
-            $("#img-box").append(`<img src="${thumb}" width="300px" height="300px"/>`);
-            n++;
-            if (n=10){return;} // doesn't break out - runs it all asynch
+        let n=0;
+        data.data.children.forEach((child)=> {
+            // while(n<10){
+            //     title = child[0].data.title;
+            //     thumb = child[0].data.thumbnail;
+            //     console.log(title);
+            //     console.log(thumb);
+            //     $("body").append(`<h1>${title}</h1>`);
+            //     $("#img-box").append(`<img src="${thumb}" width="300px" height="300px"/>`);
+            //     n++;
+            // }
+                title = child.data.title;
+                thumb = child.data.thumbnail;
+                link = child.data.permalink;
+                console.log(link);
+                $("body").append(`<h1>${title}</h1>`)
+                .css("background-color", `#${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`)
+                .css("color", `#${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`);
+                // I was trying to do random colors for each post, but it seems to do it for all.
+
+                $("body").append(`<img src="${thumb}" width="300px" height="300px"/>`);
+                $("body").append(`<a href="reddit.com${link}>Click Here for OP</a>`);
         });
-    }
-)});
+    });
+
+    $(function(){
+        $("h1").on("mouse-enter",function(){
+            $(this)
+            .css("background-color", `#${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`)
+            .css("color", `#${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`);
+        });
+    });
+
+    $(function(){
+        $("h1").on("mouse-exit",function(){
+            $(this)
+            .css("background-color", "white")
+            .css("color", "black");
+        });
+    });
+
+    $("h1").hover(
+        function(){
+            $(this)
+            .css("background-color", `#${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`)
+            .css("color", `#${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`);
+        }, function(){
+            $(this)
+            .css("background-color", "white")
+            .css("color", "black");
+        }
+    );
+
+    $("img").hover(
+        function(){
+            $(this)
+            .css("cursor", "zoom-in")
+            .css("width", "1000px")
+            .css("height", "1000px");
+        }, function(){
+            $(this)
+            .css("cursor", "default")
+            .css("width", "300px")
+            .css("height", "300px");
+        }
+    );
+});
